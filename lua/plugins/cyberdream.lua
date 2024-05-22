eca56cc07925f5c968d2ffd6c46910af16321f91
Bernardo Rezende <bernardo.rezende@sejaefi.com.br>
2024-05-22 10:46:09 +0000

    initial commit

---
+++ lua/plugins/cyberdream.lua
@@ -0,0 +1,9 @@
+return {
+  "scottmckendry/cyberdream.nvim",
+  lazy = false,
+  name = "cyberdream",
+  priority = 1000,
+  config = function()
+    vim.cmd("colorscheme cyberdream")
+  end
+}
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({ ensure_installed = { "lua_ls", "tsserver", "intelephense" } })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      lspconfig.lua_ls.setup({ capabilities = capabilities, settings = { Lua = { diagnostics = { globals = { "vim" } } } } })
+      lspconfig.tsserver.setup({ capabilities = capabilities })
+      lspconfig.intelephense.setup({ capabilities = capabilities })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end
+  }
+}
