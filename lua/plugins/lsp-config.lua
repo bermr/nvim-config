1b1ea4aee4519db4b60586fdeade567142761c95
Bernardo Rezende <bernardo.rezende@sejaefi.com.br>
2025-02-07 12:39:10 +0000

    instala lsp de go

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,7 +8,7 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
-      require("mason-lspconfig").setup({ ensure_installed = { "lua_ls", "ts_ls", "intelephense" } })
+      require("mason-lspconfig").setup({ ensure_installed = { "lua_ls", "ts_ls", "intelephense", "gopls" } })
     end
   },
   {
@@ -17,8 +17,9 @@ return {
       local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       lspconfig.lua_ls.setup({ capabilities = capabilities, settings = { Lua = { diagnostics = { globals = { "vim" } } } } })
-      lspconfig.tsserver.setup({ capabilities = capabilities })
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
       lspconfig.intelephense.setup({ capabilities = capabilities })
+      lspconfig.gopls.setup({ capabilities = capabilities })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
