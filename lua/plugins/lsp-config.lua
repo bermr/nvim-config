f9d0b17bcf1088e517ec63e381134f987d964b5b
Bernardo Rezende <bernardo.rezende@sejaefi.com.br>
2025-04-17 11:27:08 +0000

    adiciona mapping pra visualizar mensagens de erro

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,6 +23,7 @@ return {
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', vim.opts)
     end
   }
 }
